2d1ba3a963083f09e51f3bbc931aa3428b7dbb10
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-08-18 16:01:32 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,16 @@
+a = 11
+b = 3
+c = 8
+d = 2
+
+print( (a > b) and (c < d) )
+print( (a > b) or (c < d) )
+print(not((a > b) or (c < d)) )
+
+username = 'Angela'
+password = 'Gela0311'
+
+print((username == 'Angela') and (password == 'Gela0311'))
+print((username == 'Angela') or (password == 'Gela0311'))
+print(not((username == 'Angela') or (password == 'Gela0311')))
+
