f3bf3d5b2a20e66439d2aedd552cdb1f09f37d55
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-08-27 15:34:33 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Gela"
+password = "Angela11"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if uname == username and pword == password:	 
+	 print("Successfully login")
+else:
+	 print("Try again")
