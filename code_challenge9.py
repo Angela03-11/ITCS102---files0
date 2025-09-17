c1d96d2008a5db6b3f47388826a5e1e3e8b21e75
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-09-17 14:55:59 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,10 @@
+print("🦜 PARROT SIMULATOR—THE ECHO CHAMBER!")
+mock = input("What do you want your parrot to say? ")
+times = eval(input("How many times should the parrot squawk it? "))
+bird = 'Squawk!'
+
+print("Listen to your parrot: ")
+
+for haha in range(times):
+    print(bird, mock)
+    
