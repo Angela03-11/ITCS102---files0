7842b35d559c62a9605d9412b70d03c1953cf3da
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-09-24 17:05:34 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,44 @@
+#String Concatenation 
+
+first = 'Angela Nicole'
+middle = 'Buitizon'
+last = 'Quejano'
+
+print("My name is " , first," ", middle," ",last)
+
+
+
+fn = 'Angela Nicole'
+mn = 'Buitizon'
+ln = 'Quejano'
+
+print(f"My name is {fn} {mn} {ln} ")
+
+
+
+#For Uppercase 
+fnm = 'Angela Nicole'
+mnm = 'Buitizon' 
+lnm = 'Quejano' 
+
+print(f"My name is {fnm.upper()} {mnm.upper()} {lnm.upper()} {10 + 8}") 
+
+
+#For Lowercase 
+una = 'Angela Nicole' 
+gitna = 'Buitizon' 
+huli = 'Quejano' 
+
+print(f"My name is {una.lower()} {gitna.lower()} {huli.lower()} {10 + 8}") 
+
+
+
+#Summation
+sum = 0
+for add in range(1, 11, 1):
+    num = eval(input(f" {add} - Input any number: "))
+    sum += num
+print(f"The total sum of all the numbers is {sum} ")
+
+
+ 
