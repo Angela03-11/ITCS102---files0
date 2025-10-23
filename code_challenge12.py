dd92fb2820518bb3991052b36c99e73ef60555dc
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-10-23 12:02:51 +0800

    Add files via upload

---
+++ code_challenge12.py
@@ -0,0 +1,11 @@
+for gela in range(1, 7, 1):
+    for kris in range(6, gela, -1):
+        print(" ", end=" ")
+    for trix in range(gela, 0, -1):
+        print(trix, end=" ")
+    for nick in range(2, gela + 1, 1):
+        print(nick, end=" ")
+    print()
+    
+    
+   
