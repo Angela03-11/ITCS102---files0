864bbcd6635a05403cd682b62261aa33942527b7
Quejano-Angela ITCS102_BSIT1B <angelaquejano09@gmail.com>
2025-12-14 00:41:55 -0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,16 @@
+# Dictionary
+
+cats = ['Persian', 'Scottish', 'Ginger', 'Siamese', 'MingMing']
+
+print(cats[3])
+
+#{Keys : values, Keys : Values}
+cat_traits = {'color': 'White', 'breed': 'Persian', 'size': 'Small', 'behavior': 'Playful', 'fur': 'Fluffy', 'eyes': 'Blue'}
+print(cat_traits['fur'])
+
+cat_traits['smell'] = 'Mabaho'
+print(cat_traits)
+
+cat_traits.pop('size')
+print(cat_traits)
+
